6adfc14512ecc827b6d1369224846b2bed1e582b
Tyler Holzer <tholzer@purdue.edu>
2011-01-13 12:31:17 -0500

    Added a new print statement with the words "Waka Waka"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 		System.out.println("Just another test.");
-System.out.println("Jaye was here.");
+		System.out.println("Jaye was here.");
+		System.out.println("Waka Waka");
 	}
 
 }
